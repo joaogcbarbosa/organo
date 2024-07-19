66dd80c35372e7f9922435bd5bfa9e39bb364479
joaogcbarbosa <joaogcb29@gmail.com>
2024-07-19 19:58:08 -0300

    Chore: field component

---
+++ src/components/CampoTexto/index.jsx
@@ -0,0 +1,12 @@
+import "./CampoTexto.css"
+
+const CampoTexto = (props) => {
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input value={props.valor} onChange={(e) => props.aoAlterar(e.target.value)} required={props.obrigatorio} placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default CampoTexto;
