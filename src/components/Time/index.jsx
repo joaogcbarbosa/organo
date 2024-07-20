ac5d15ed0f9abec6188903ae20b38b9911ef13f3
joaogcbarbosa <joaogcb29@gmail.com>
2024-07-20 10:56:17 -0300

    Chore: mapping colaboradores

--- src/components/Time/index.jsx
+++ src/components/Time/index.jsx
@@ -1,3 +1,4 @@
+import Colaborador from "../Colaborador";
 import "./Time.css"
 
 const Time = (props) => {
@@ -10,6 +11,11 @@ const Time = (props) => {
     return (
         <section className="time" style={ {backgroundColor: css.corSecundaria} }>
             <h3 style={ {borderColor: css.corPrimaria} }>{props.nome}</h3>
+            <div className="colaboradores">
+                {props.colaboradores.map(colaborador => (
+                    <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>
+                ))}
+            </div>
         </section>
     )
 }
