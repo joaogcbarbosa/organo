e9f4bd2eb0fb644e68a4e5f8b2f60d78eb1bdde4
joaogcbarbosa <joaogcb29@gmail.com>
2024-07-22 17:32:25 -0300

    Feat: Rodape component

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,18 @@
+import "./Rodape.css"
+
+const Rodape = () => {
+    return (
+        <div className="rodape">
+            <div className="organo">
+                <img src="/images/logo.png" alt="Logo" />
+            </div>
+            <div className="midia">
+                <img src="/images/fb.png" alt="Facebook" />
+                <img src="/images/tw.png" alt="Twitter" />
+                <img src="/images/ig.png" alt="Instagram" />
+            </div>
+        </div>
+    )
+}
+
+export default Rodape;
