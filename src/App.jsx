77db59ad8dc7c540bf231511b1a8c7a843b4d8f1
joaogcbarbosa <joaogcb29@gmail.com>
2024-07-22 17:32:03 -0300

    Chore: add Rodape component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
 import Time from './components/Time';
+import Rodape from './components/Rodape';
 
 function App() {
 
@@ -62,6 +63,7 @@ function App() {
               colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
         />
       ))}
+      <Rodape />
     </div>
   );
 }
