e0bb030d338b60a35114d978f69d5812c271db84
joaogcbarbosa <joaogcb29@gmail.com>
2024-07-23 01:37:03 -0300

    Chore: aoDeletar

--- src/components/Colaborador/index.jsx
+++ src/components/Colaborador/index.jsx
@@ -1,17 +1,20 @@
-import "./Colaborador.css"
+import "./Colaborador.css";
 
 const Colaborador = (props) => {
-    return (
-        <div className="colaborador">
-            <div className="cabecalho" style={ {backgroundColor: props.corPrimaria} }>
-                <img src={props.imagem} alt={props.nome}/>
-            </div>
-            <div className="rodape">
-                <h4>{props.nome}</h4>
-                <h5>{props.cargo}</h5>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="colaborador">
+      <div className="deletar" onClick={props.aoDeletar}>
+        deletar
+      </div>
+      <div className="cabecalho" style={{ backgroundColor: props.corPrimaria }}>
+        <img src={props.imagem} alt={props.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{props.nome}</h4>
+        <h5>{props.cargo}</h5>
+      </div>
+    </div>
+  );
+};
 
 export default Colaborador;
--- src/components/Time/index.jsx
+++ src/components/Time/index.jsx
@@ -1,23 +1,31 @@
 import Colaborador from "../Colaborador";
-import "./Time.css"
+import "./Time.css";
 
 const Time = (props) => {
+  const css = {
+    corPrimaria: props.corPrimaria,
+    corSecundaria: props.corSecundaria,
+  };
 
-    const css = {
-        corPrimaria: props.corPrimaria,
-        corSecundaria: props.corSecundaria,
-    }
-
-    return (props.colaboradores.length > 0 &&
-        <section className="time" style={ {backgroundColor: css.corSecundaria} }>
-            <h3 style={ {borderColor: css.corPrimaria} }>{props.nome}</h3>
-            <div className="colaboradores">
-                {props.colaboradores.map(colaborador => (
-                    <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} corPrimaria={css.corPrimaria}/>
-                ))}
-            </div>
-        </section>
+  return (
+    props.colaboradores.length > 0 && (
+      <section className="time" style={{ backgroundColor: css.corSecundaria }}>
+        <h3 style={{ borderColor: css.corPrimaria }}>{props.nome}</h3>
+        <div className="colaboradores">
+          {props.colaboradores.map((colaborador) => (
+            <Colaborador
+              key={colaborador.nome}
+              nome={colaborador.nome}
+              cargo={colaborador.cargo}
+              imagem={colaborador.imagem}
+              corPrimaria={css.corPrimaria}
+              aoDeletar={props.aoDeletar}
+            />
+          ))}
+        </div>
+      </section>
     )
-}
+  );
+};
 
 export default Time;
