291c79b742475ea856146fbb6150dc609155da40
joaogcbarbosa <joaogcb29@gmail.com>
2024-07-24 18:30:11 -0300

    Chore: input color button

--- src/components/Time/index.jsx
+++ src/components/Time/index.jsx
@@ -1,11 +1,12 @@
 import Colaborador from "../Colaborador";
 import "./Time.css";
 
-const Time = ( {nome, colaboradores, corPrimaria, corSecundaria, aoDeletar} ) => {
+const Time = ( {nome, colaboradores, corPrimaria, corSecundaria, aoDeletar, aoMudarCor} ) => {
   return (
     colaboradores.length > 0 && (
       <section className="time" style={{ backgroundColor: corSecundaria }}>
         <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
+        <input value={corPrimaria} onChange={aoMudarCor} type="color" className="input-color" />
         <div className="colaboradores">
           {colaboradores.map((colaborador) => (
             <Colaborador
