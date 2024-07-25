3da64ca8904451514de1b2f18308cbaf13e82aab
joaogcbarbosa <joaogcb29@gmail.com>
2024-07-25 00:50:12 -0300

    Chore: receiving id as prop

--- src/components/Colaborador/index.jsx
+++ src/components/Colaborador/index.jsx
@@ -1,10 +1,10 @@
 import "./Colaborador.css";
 import { AiFillCloseCircle } from "react-icons/ai";
 
-const Colaborador = ( {nome, cargo, imagem, corPrimaria, aoDeletar} ) => {
+const Colaborador = ( {id, nome, cargo, imagem, corPrimaria, aoDeletar} ) => {
   return (
     <div className="colaborador">
-      <AiFillCloseCircle className="deletar" onClick={aoDeletar} size={25}/>
+      <AiFillCloseCircle className="deletar" onClick={() => aoDeletar(id)} size={25}/>
       <div className="cabecalho" style={{ backgroundColor: corPrimaria }}>
         <img src={imagem} alt={nome} />
       </div>
