4cd24b395ce2c2bed0981b91ef6bf0bd943fd558
joaogcbarbosa <joao.barbosa@rioenergy.com.br>
2024-08-02 09:48:45 -0300

    Chore: only routers on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,16 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import Banner from "./components/Banner";
-import Formulario from "./components/Formulario";
-import Time from "./components/Time";
-import Rodape from "./components/Rodape";
-import { v4 as uuidv4 } from 'uuid';
-
-const REACT_APP_API_URL = "http://localhost:8080/colaboradores"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Organo from "./pages/Organo";
 
 function App() {
-  const [times, setTime] = useState([
-    { id: uuidv4(),
-      nome: "Programação",
-      cor: "#57C278",
-    },
-    { id: uuidv4(),
-      nome: "Front-End",
-      cor: "#82CFFA",
-    },
-    { id: uuidv4(),
-      nome: "Data Science",
-      cor: "#A6D157",
-    },
-    { id: uuidv4(),
-      nome: "Devops",
-      cor: "#E06B69",
-    },
-    { id: uuidv4(),
-      nome: "UX e Design",
-      cor: "#DB6EBF",
-    },
-    { id: uuidv4(),
-      nome: "Mobile",
-      cor: "#FFBA05",
-    },
-    { id: uuidv4(),
-      nome: "Inovação e Gestão",
-      cor: "#FF8A29",
-    },
-  ]);
-
-  const [colaboradores, setColaboradores] = useState([]);
-
-  useEffect(() => {
-    fetch(REACT_APP_API_URL)
-      .then(response => response.json())
-      .then(dados => setColaboradores(dados))
-  }, [])
-
-  const cadastraNovoColaborador = (colaborador) => {
-    setColaboradores([...colaboradores, colaborador]);
-  };
-
-  const cadastraNovoTime = (time) => {
-    setTime([...times, time]);
-  };
-
-  const deletarColaborador = (id) => {
-    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
-  };
-
-  const favoritarColaborador = (id) => {
-    setColaboradores(colaboradores.map(colaborador => {
-      if (colaborador.id === id) {
-        colaborador.favorito = !colaborador.favorito;
-      }
-      return colaborador;
-    }))
-  };
-
-  const mudarCorTime = (nome, cor) => {
-    setTime(times.map(time => {
-      if (time.nome === nome) {
-        time.cor = cor;
-      }
-      return time;
-    }))
-  }
 
   return (
     <div className="App">
-      <Banner />
-      <Formulario
-        aoCadastrarColaborador={cadastraNovoColaborador}
-        aoCadastrarTime={cadastraNovoTime}
-        times={times.map((time) => time.nome)}
-      />
-      {times.map((time) => (
-        <Time
-          key={time.nome}
-          nome={time.nome}
-          cor={time.cor}
-          colaboradores={colaboradores.filter(
-            (colaborador) => colaborador.time === time.nome
-          )}
-          aoDeletar={deletarColaborador}
-          aoMudarCor={mudarCorTime}
-          aoFavoritar={favoritarColaborador}
-        />
-      ))}
-      <Rodape />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Organo />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
