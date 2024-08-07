bd79dea0790243a0f129466638a34ddad9e8a05f
joaogcbarbosa <joao.barbosa@rioenergy.com.br>
2024-08-07 18:06:20 -0300

    Chore: using contexts

--- src/pages/Organo/index.jsx
+++ src/pages/Organo/index.jsx
@@ -2,29 +2,13 @@ import Banner from "../../components/Banner";
 import Rodape from "../../components/Rodape";
 import Formulario from "../../components/Formulario";
 import Time from "../../components/Time";
-import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { useContext } from "react";
+import { TimeContext } from "../../context/TimeContext";
+import { ColaboradorContext } from "../../context/ColaboradorContext";
 
 const Organo = () => {
-  const REACT_APP_API_URL = "http://localhost:8080/colaboradores";
-
-  const [times, setTimes] = useState([
-    { id: uuidv4(), nome: "Programação", cor: "#57C278" },
-    { id: uuidv4(), nome: "Front-End", cor: "#82CFFA" },
-    { id: uuidv4(), nome: "Data Science", cor: "#A6D157" },
-    { id: uuidv4(), nome: "Devops", cor: "#E06B69" },
-    { id: uuidv4(), nome: "UX e Design", cor: "#DB6EBF" },
-    { id: uuidv4(), nome: "Mobile", cor: "#FFBA05" },
-    { id: uuidv4(), nome: "Inovação e Gestão", cor: "#FF8A29" },
-  ]);
-
-  const [colaboradores, setColaboradores] = useState([]);
-
-  useEffect(() => {
-    fetch(REACT_APP_API_URL)
-      .then((response) => response.json())
-      .then((dados) => setColaboradores(dados));
-  }, []);
+  const {colaboradores, setColaboradores} = useContext(ColaboradorContext)
+  const {times, setTimes} = useContext(TimeContext)
 
   const cadastraNovoColaborador = (colaborador) => {
     setColaboradores([...colaboradores, colaborador]);
@@ -32,34 +16,6 @@ const Organo = () => {
 
   const cadastraNovoTime = (time) => {
     setTimes([...times, time]);
-  };
-
-  const deletarColaborador = (id) => {
-    setColaboradores(
-      colaboradores.filter((colaborador) => colaborador.id !== id)
-    );
-  };
-
-  const favoritarColaborador = (id) => {
-    setColaboradores(
-      colaboradores.map((colaborador) => {
-        if (colaborador.id === id) {
-          colaborador.favorito = !colaborador.favorito;
-        }
-        return colaborador;
-      })
-    );
-  };
-
-  const mudarCorTime = (nome, cor) => {
-    setTimes(
-      times.map((time) => {
-        if (time.nome === nome) {
-          time.cor = cor;
-        }
-        return time;
-      })
-    );
   };
 
   return (
@@ -78,9 +34,6 @@ const Organo = () => {
           colaboradores={colaboradores.filter(
             (colaborador) => colaborador.time === time.nome
           )}
-          aoDeletar={deletarColaborador}
-          aoMudarCor={mudarCorTime}
-          aoFavoritar={favoritarColaborador}
         />
       ))}
       <Rodape />
