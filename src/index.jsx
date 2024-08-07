49b08ee3a7da3a33c9fa5453242b73de46b70ba3
joaogcbarbosa <joao.barbosa@rioenergy.com.br>
2024-08-07 18:04:57 -0300

    Chore: add providers

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { TimeProvider } from './context/TimeContext';
+import { ColaboradorProvider } from './context/ColaboradorContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <TimeProvider>
+    <ColaboradorProvider>
+      <App />
+    </ColaboradorProvider>
+  </TimeProvider>
 );
